53a5002a3884ba06a3258807c1096d1a075f7e11
Ramo <ramoalikulov77@gmail.com>
2025-01-11 02:13:07 +0600

    fixed bugs

--- src/components/ConditionsSection.jsx
+++ src/components/ConditionsSection.jsx
@@ -4,12 +4,12 @@ const conditions = [
 	{
 		title: 'Оформление «Кис АРТ»',
 		desc: 'Помощь с документами',
-		price: '2000₽',
+		price: '2000P',
 	},
 	{
 		title: 'Путевые листы',
 		desc: 'Поддержка с оформлением',
-		price: '5₽',
+		price: '5P',
 	},
 	{
 		title: 'Удобный вывод средств',
@@ -98,7 +98,7 @@ const Conditions = () => {
 								<h4 className='min-2sm:text-xl'>Для парковых водителей:</h4>
 								<div className='flex justify-between'>
 									<p className='min-2sm:text-sm text-xs font-normal'>
-										За смену 100₽ и 1₽ с каждого заказа
+										За смену 100P и 1P с каждого заказа
 									</p>
 								</div>
 							</div>
@@ -107,7 +107,7 @@ const Conditions = () => {
 
 								<div className='flex justify-between'>
 									<p className='min-2sm:text-sm text-xs font-normal'>
-										За смену 50₽ и 1₽ с каждого заказа
+										За смену 50P и 1P с каждого заказа
 									</p>
 								</div>
 							</div>
@@ -115,8 +115,8 @@ const Conditions = () => {
 						<div className='w-3/4 lg:w-3/4 xl:w-11/12 text-right'>
 							<h5 className='text-sm lg:text-xs 3sm::font-normal font-semibold'>
 								Например, вы отработали смену и выполнили 20 заказов. При этом
-								комиссия таксопарка составит 120 ₽, а для самозанятых,
-								сотрудничающих через таксопарк, она будет всего 70 ₽. Комиссия
+								комиссия таксопарка составит 120P, а для самозанятых,
+								сотрудничающих через таксопарк, она будет всего 70P. Комиссия
 								взимается только в те дни, когда вы выполняете заказы.
 							</h5>
 						</div>
